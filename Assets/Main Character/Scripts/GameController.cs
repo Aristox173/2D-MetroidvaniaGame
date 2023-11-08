23738ec38ce8690d0fdfaf5c7e25f89d1f5deb19
Juan Aristizábal <juanaristi2001@gmail.com>
2023-11-08 14:45:54 -0500

    Add Checkpoint System

---
+++ Assets/Main Character/Scripts/GameController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.Examples;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    Vector2 checkpointPos;
+    Rigidbody2D playerRb;
+    TrailRenderer trailRenderer;
+    [SerializeField] private ParticleSystem testParticleSystem = default;
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        trailRenderer = GetComponent<TrailRenderer>();
+    }
+
+    private void Start()
+    {
+        checkpointPos = transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Spike"))
+        {
+            Die();
+        }
+    }
+
+    public void UpdateCheckpoint(Vector2 pos)
+    {
+        checkpointPos = pos;
+    }
+
+    void Die()
+    {
+        StartCoroutine(Respawn(0.5f));
+    }
+
+    IEnumerator Respawn(float duration)
+    {
+        testParticleSystem.Play();
+        playerRb.velocity = new Vector2(0, 0);
+        playerRb.simulated = false;
+
+        trailRenderer.enabled = false;
+
+        transform.localScale = new Vector3(0, 0, 0);
+        yield return new WaitForSeconds(duration);
+        transform.position = checkpointPos;
+        transform.localScale = new Vector3(1, 1, 1);
+
+        trailRenderer.enabled = true;
+
+        playerRb.simulated = true;
+        
+    }
+}
--- Assets/Main Character/Scripts/Player.cs
+++ Assets/Main Character/Scripts/Player.cs
@@ -15,6 +15,8 @@ public class Player : MonoBehaviour
 
     #region Numeric Values
 
+    private int facingDir = 1;
+
     [Header("Movement Info")]
     [SerializeField]
     private float moveSpeed;
@@ -22,6 +24,24 @@ public class Player : MonoBehaviour
     private float jumpForce;
     private float xInput;
 
+    [Header("Dash Info")]
+    [SerializeField]
+    private float dashSpeed;
+    [SerializeField]
+    private float dashDuration;
+    private float dashTime;
+
+    [SerializeField]
+    private float dashCooldown;
+    private float dashCooldownTimer;
+    private bool canDash = true; // Flag to control dashing
+
+    [Header("Ground Check")]
+    [SerializeField]
+    private float groundCheckDistance;
+    [SerializeField]
+    private LayerMask whatIsGround;
+    private bool isGrounded;
     #endregion
 
     #region Boolean Values
@@ -41,6 +61,14 @@ public class Player : MonoBehaviour
         Movement();
         Flip();
         AnimatorController();
+
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+
+        if (isGrounded)
+            canDash = true;
+
+        dashTime -= Time.deltaTime;
+        dashCooldownTimer -= Time.deltaTime;
     }
 
     private void CheckInput()
@@ -51,11 +79,26 @@ public class Player : MonoBehaviour
         {
             Jump();
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            Dash();
+        }
     }
 
     private void Movement()
     {
-        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        if (dashTime > 0)
+        {
+            rb.velocity = new Vector2(facingDir * dashSpeed, 0); // Dash horizontally with y velocity set to 0
+            rb.gravityScale = 0; // Disable gravity during the dash
+        }
+        else
+        {
+            rb.gravityScale = 9.8f; // Reset gravity scale to its normal value
+            rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        }
+
     }
 
     private void Flip()
@@ -63,16 +106,29 @@ public class Player : MonoBehaviour
         if (xInput < 0)
         {
             spriteRenderer.flipX = true;
+            facingDir = -1;
         }
         else if (xInput > 0)
         {
             spriteRenderer.flipX = false;
+            facingDir = 1;
         }
     }
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (isGrounded)
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void Dash()
+    {
+        if (dashCooldownTimer < 0)
+        {
+            dashCooldownTimer = dashCooldown;
+            dashTime = dashDuration;
+            canDash = false; // Disable dashing until the player is grounded
+        }
     }
 
     private void AnimatorController()
@@ -83,4 +139,8 @@ public class Player : MonoBehaviour
         animator.SetBool("isMoving", isMoving);
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
+    }
 }
